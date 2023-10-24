6d0c7fc0756aaa191a43fbc3f46feedd1b6a52b4
gizeli-pereira <gizeli.pereira1@gmail.com>
2023-10-24 07:55:38 -0500

    update

--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -1,9 +1,13 @@
-import Image from 'next/image'
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from "next/navigation";
 
-export default function Home() {
+
+async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
     </main>
   )
 }
+
+export default Home;
