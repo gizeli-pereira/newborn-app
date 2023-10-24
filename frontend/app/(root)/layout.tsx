a414f289e7439cfee0e0616f141a92157c34cfd8
gizeli-pereira <gizeli.pereira1@gmail.com>
2023-10-24 08:02:56 -0500

    started bar components

--- frontend/app/(root)/layout.tsx
+++ frontend/app/(root)/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs';
+import { dark } from "@clerk/themes";
 import '../globals.css'
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,11 +18,30 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang='en'>
+        <body className={inter.className}>
+          <Topbar />
+
+          <main className='flex flex-row'>
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            {/* @ts-ignore */}
+            <RightSidebar />
+          </main>
+
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
